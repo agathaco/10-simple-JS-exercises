0548b93b7ab3b6a516d8d1fa075a1c57a125018e
Agathe Cocco <agathaco@hotmail.com>
2017-03-27 21:42:04 +0100

    added exercise #1

---
+++ exercise-1.js
@@ -0,0 +1,9 @@
+// Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.
+function max(number1, number2) {
+  if (number1 > number2) {
+  return number1
+  } else {
+  return number2
+  }
+};
+max(38,49)
