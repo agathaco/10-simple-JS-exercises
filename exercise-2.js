363fbf3736f9cf7cb53b8adb173a739bebe83522
Agathe Cocco <agathaco@hotmail.com>
2017-03-27 21:48:58 +0100

    added exercise #2

---
+++ exercise-2.js
@@ -0,0 +1,7 @@
+
+//Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
+function maxOfThree(number1, number2, number3) {
+   return Math.max(number1, number2, number3)
+ }
+maxOfThree(2,43,98)
+
